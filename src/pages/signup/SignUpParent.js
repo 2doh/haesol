6717d758eadb08257dcd58166257a3503cece7c4
2doh <ldh0973@naver.com>
2024-07-02 16:53:27 +0900

    add : signup 페이지 추가

--- src/pages/signup/SignUpParent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignUpParent = () => {
-  return <div>SignUpParent</div>;
-};
-
-export default SignUpParent;
--- src/pages/signup/SignUpStaff.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignUpStaff = () => {
-  return <div>SignUpStaff</div>;
-};
-
-export default SignUpStaff;
