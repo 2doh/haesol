016f2c7397bf92c9217b338fd7fd662a37e36ae1
suminkim8203 <kimsm8203@gmail.com>
2024-07-04 17:53:10 +0900

    feat : 학생 신상 정보 관리 페이지 제작

--- src/pages/student/StudentsList.js
+++ src/pages/student/StudentsList.js
@@ -1,74 +1,123 @@
 import React, { useState } from "react";
-import "../../scss/student/studentList.scss";
+import "../../scss/student/studentList.css";
 import { useNavigate } from "react-router";
+import styled from "@emotion/styled";
 const StudentsList = () => {
   // 반 정보
-  const [gradeClass, setGradeClass] = useState("5학년 7반");
-  // 학생 리스트 정보
-  const [studentNumber, setStudentNumber] = useState(1);
-  const [studentName, setStudentName] = useState("김누구");
-  const [studentGender, setStudentGendr] = useState("여자");
-  const [studentBirth, setStudentBirth] = useState("010102");
-  const [studentPhone, setStudentPhone] = useState("010-1234-5687");
-  const [parentsName, setParentsName] = useState("김구누");
-  const [parentsPhone, setParentsPhone] = useState("010-9876-5612");
+  const gradeClass = "5학년 7반";
+
+  // 학생 더미 데이터
+  const studentsList = [
+    {
+      studentNumber: "1",
+      studentName: "김누구",
+      studentGender: "여자",
+      studentBirth: "010102",
+      studentPhone: "123-456-7890",
+      parentsName: "김구누",
+      parentsPhone: "987-654-3210",
+    },
+    {
+      studentNumber: "2",
+      studentName: "니누구",
+      studentGender: "남자",
+      studentBirth: "010101",
+      studentPhone: "234-567-8901",
+      parentsName: "니구누",
+      parentsPhone: "876-543-2109",
+    },
+  ];
 
   const navigate = useNavigate();
   const handleClick = () => {
     navigate(`/students/edit`);
   };
 
+  const StudentsListStyle = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  `;
+
   return (
     <div className="main-core">
       <div className="student-list-title">
-        {/* <!-- 제목 위치 --> */}
+        {/* 제목 위치 */}
         <span>{gradeClass}</span>
       </div>
-      {/* <!-- 리스트 --> */}
-      <div className="list-wrap">
-        {/* <!-- 리스트 타이틀 --> */}
-        <div className="list-title">
-          <ul>
-            <li>번호</li>
-            <li>이름</li>
-            <li>성별</li>
-            <li>생년월일</li>
-            <li>전화번호</li>
-            <li>학부모</li>
-            <li>학부모 전화번호</li>
-          </ul>
+      <StudentsListStyle>
+        <div className="grid-frame">
+          <div className="item">
+            <div className="grid-inner">
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">번호</div>
+              </div>
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">이름</div>
+              </div>
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">성별</div>
+              </div>
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">생년월일</div>
+              </div>
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">학생 전화번호</div>
+              </div>
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">학부모</div>
+              </div>
+              <div className="grid-inner-item">
+                <div className="grid-inner-item-text">학부모 전화번호</div>
+              </div>
+            </div>
+          </div>
+          {studentsList.map((item, index) => (
+            <div className="item" key={index}>
+              <div
+                className="grid-inner"
+                onClick={() => {
+                  handleClick();
+                }}
+                id="grid-content"
+              >
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">
+                    {item.studentNumber}
+                  </div>
+                </div>
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">{item.studentName}</div>
+                </div>
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">
+                    {item.studentGender}
+                  </div>
+                </div>
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">
+                    {item.studentBirth}
+                  </div>
+                </div>
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">
+                    {item.studentPhone}
+                  </div>
+                </div>
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">{item.parentsName}</div>
+                </div>
+                <div className="grid-inner-item">
+                  <div className="grid-inner-item-text">
+                    {item.parentsPhone}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-        {/* <!-- 학생 리스트 --> */}
-        {/* map 돌려서 아래로 추가되게 해야할것같다... 
-        컴포넌트로 뽑아야 하나? 고민...*/}
-        <div
-          className="list-content"
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          <ul>
-            <li>{studentNumber}번</li>
-            <li>{studentName}</li>
-            <li>{studentGender}</li>
-            <li>{studentBirth}</li>
-            <li>{studentPhone}</li>
-            <li>{parentsName}</li>
-            <li>{parentsPhone}</li>
-          </ul>
-        </div>
-        <div className="list-content">
-          <ul>
-            <li>{studentNumber}번</li>
-            <li>{studentName}</li>
-            <li>{studentGender}</li>
-            <li>{studentBirth}</li>
-            <li>{studentPhone}</li>
-            <li>{parentsName}</li>
-            <li>{parentsPhone}</li>
-          </ul>
-        </div>
-      </div>
+      </StudentsListStyle>
     </div>
   );
 };
