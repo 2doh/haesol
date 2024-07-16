e8d5d9f22c8c0bd7a9b4b340139e6a956352a798
suminkim8203 <kimsm8203@gmail.com>
2024-07-16 23:34:48 +0900

    feat : 학생 정보 처리 이미지 관련 구현중

--- src/pages/student/StudentImg.js
+++ src/pages/student/StudentImg.js
@@ -1,11 +1,18 @@
 import styled from "@emotion/styled";
+import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 
 const StudentImg = ({ studentPic, setStudentPic, studentPk }) => {
-  // const [sendFile, setSendFile] = useState(null);
-  const [previewFile, setPreviewFile] = useState("");
+  // 파일 선택 태그
   const fileBt = useRef(null);
-  //  const stu_id = 2;
+
+  // 이미지 미리보기 url 관리
+  const [previewFile, setPreviewFile] = useState("");
+
+  // 이미지 미리보기 JSX
+  // const makeThumbnail = () => {
+  //   <img src={previewFile} />;
+  // };
 
   // 강제로 input type="file" 을 클릭한 것처럼 js 에서 실행
   const handleFileClick = () => {
@@ -15,16 +22,29 @@ const StudentImg = ({ studentPic, setStudentPic, studentPk }) => {
   // 파일 목록 및 이미지 미리보기
   const handleFileChange = e => {
     const file = e.target.files[0];
-    const url = URL.createObjectURL(file);
     // 파일 보관
-    setStudentPic(file);
+    setSendFile(file);
+
+    const url = URL.createObjectURL(file);
     // 웹 브라우저 임시 파일 주소
     setPreviewFile(url);
   };
 
+  // // 이미지 미리보기
+  // const handleFile = e => {
+  //   const file = e.target.files[0];
+
+  // };
+
   useEffect(() => {
+    if (studentPic) {
+      setPreviewFile(
+        `http://112.222.157.156:5121/pic/2nd/student/${studentPk}/${studentPic}`,
+      );
+    }
     console.log(studentPic);
-  }, []);
+    console.log(previewFile);
+  }, [studentPic, previewFile]);
 
   const StudentsImeStyle = styled.div`
     width: 100%;
@@ -46,11 +66,7 @@ const StudentImg = ({ studentPic, setStudentPic, studentPk }) => {
     // 이미지 전체 영역
     <StudentsImeStyle onClick={() => handleFileClick()}>
       {/* 이미지 보여지는 곳 */}
-      <div className="img-contain">
-        <img
-          src={`http://192.168.0.164:8080/pic/2nd/student/${studentPk}/47872175-b41f-4080-bcf9-dc72604c46d5.png`}
-        />
-      </div>
+      <img src={previewFile} alt="Student" />;
       <input
         style={{ display: "none" }}
         id="filebt_id"
